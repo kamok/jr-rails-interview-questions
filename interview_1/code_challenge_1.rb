1247dd10b718a42e54ef68ff4ef54b5590cf95a5
Ka_Lenovo <heykamok@gmail.com>
2016-06-29 22:48:21 -0400

    Tighten format

--- interview_1/code_challenge_1.rb
+++ interview_1/code_challenge_1.rb
@@ -4,39 +4,39 @@
 # Without .min, .max, or .sort
 # { min: -1, max:12}
 
-	# def calc_max_min(array)
-	# 	min = array[0]
-	# 	max = array[0]
-	# 		array.map do |value|
-	# 			if value <= min 
-	# 				min = value
-	# 			elsif value >= max
-	# 				max = value
-	# 			end
-	# 		end
-	# 	a = {}
-	# 	a[:min] = min
-	# 	a[:max] = max
-	# 	a
-	# end
+	def calc_max_min(array)
+		min, max = array[0], array[0]
+			array.map do |value|
+				if value <= min 
+					min = value
+				elsif value >= max
+					max = value
+				end
+			end
+		a = {}
+		a[:min] = min
+		a[:max] = max
+		a
+	end
 
-	# p calc_max_min([ 1, 0, 5, 2, 10, 8, 12, -1 ])
+	p calc_max_min([ 1, 0, 5, 2, 10, 8, 12, -1 ])
 
 # Imagine our "%" is broken. Write a function that returns the remainder of division of the dividend by the divisor:
 # remainder( 10, 3 ) = 1
 # remainder( 12, 4 ) = 0
 # Notice: I didn't finish this challenge on time, but the point is the logic behind it.
-	def remainder(dividend,divisor)
-		@remainder = dividend - divisor
-		loop do 
-			@remainder -= divisor
-			break if @remainder == 0 || @remainder < 0
-		end	
-		if @remainder == 0
-			return 0
-		elsif @remainder < 0
-			return @remainder + divisor
-		end
-	end
 
-	p remainder(12,4)
+	# def remainder(dividend,divisor)
+	# 	@remainder = dividend - divisor
+	# 	until @remainder <= 0
+	# 		@remainder -= divisor
+	# 	end	
+
+	# 	puts case 
+	# 		when @remainder == 0 then 0
+	# 		when @remainder < 0 then @remainder + divisor
+	# 	end
+	# end
+
+	# remainder(12,4)
+	# remainder(10,3)
